9bc562efa6cda9ca2fa15c5fd4beae6d371c095d
andrew2719 <andrewblaze2719@gmail.com>
2023-10-25 22:04:47 +0530

    free space calculation for both the windows and posix are updated

--- settings.py
+++ settings.py
@@ -12,6 +12,15 @@ APP_LOG = os.path.join(DFS_MAIN_PATH, 'app.log')
 CLIENT_LOG = os.path.join(DIR_PATH,'Client','client.log')
 # print(CLIENT_LOG)
 
+# get the OS type mac or windows or linux
+OS_TYPE = os.name
 
-disk_usage = psutil.disk_usage('C:\\')
+disk_usage = ''
+# if os is linux or mac
+if OS_TYPE == 'posix':
+    disk_usage = psutil.disk_usage('/')
+else:
+    disk_usage = psutil.disk_usage('C:\\')
+# disk_usage = psutil.disk_usage('C:\\')
 FREE_SPACE = disk_usage.free
+# print(FREE_SPACE)
