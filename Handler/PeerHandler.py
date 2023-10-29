7a0995bd3c045fb4f721d498be055d1151bd7b90
andrew2719 <andrewblaze2719@gmail.com>
2023-10-29 21:13:29 +0530

    peer handler skeliton is made , need to make further changes later pusing for now at initial stage

--- Handler/PeerHandler.py
+++ Handler/PeerHandler.py
@@ -1,76 +1,82 @@
 import asyncio
 import hashlib
-import Responses
 
 class Distributor:
-    def __init__(self,peer_connections,table):
-        self.peers = peer_connections # has ([ip] = (reader,writer))
-        self.table = table
+    def __init__(self, peer_connections):
+        self.peers = peer_connections
         self.MAX_RETRIES = 3
         self.BACKOFF_INITIAL_DELAY = 1  # in seconds
         self.node_metadata = {}
-        for i in self.peers:
-            self.node_metadata[i] = {'success_rate': 0}
-        self.busy_nodes = set()
-    
-    async def send_chunk_to_peer(self, chunk, expected_hash, ip):
-        # This is a mock function for demonstration purposes. Replace with actual sending logic.
-        writer = None  # You should create the writer here
-        await writer.write(chunk)
+        for ip, (reader, writer) in self.peers.items():
+            self.node_metadata[ip] = {'success_rate': 0, 'retries': 0}
+        self.busy_nodes = {ip: False for ip in self.peers.keys()}
+        # self.table = None
 
-        # For demonstration purposes, we'll simulate the hash check
-        received_hash = hashlib.sha256(chunk.encode()).hexdigest()
+    def get_backoff_delay(self, attempt):
+        return min(self.MAX_RETRIES, self.BACKOFF_INITIAL_DELAY * (2 ** attempt))
 
-        if received_hash == expected_hash:
-            # Just a mock to simulate success rate.
-            # Replace this logic with actual communication and validation with the node
-            if node_metadata[ip]['success_rate'] > 50:
-                return True
-            else:
-                return False
-        return False
+    async def distribute_chunk_to_node(self, idx, data):
+        attempt = 0
+        while attempt < self.MAX_RETRIES:
+            available_nodes = [ip for ip, is_busy in self.busy_nodes.items() if not is_busy]
+            if not available_nodes:
+                await asyncio.sleep(self.BACKOFF_INITIAL_DELAY)
+                continue
 
-    def get_best_available_node(self):
-        sorted_peers = sorted(self.peers, key=lambda ip: node_metadata[ip]['success_rate'], reverse=True)
-        for peer in sorted_peers:
-            if peer not in busy_nodes:
-                return peer
-        return None
+            ip = available_nodes[0]
+            reader, writer = self.peers[ip]
+            self.busy_nodes[ip] = True
 
-    def distribute_chunk_to_all_peers(self, chunk, expected_hash):
-        best_node = self.get_best_available_node()
-        if best_node:
-            busy_nodes.add(best_node)
-            asyncio.create_task(self.distribute_chunk_to_node(chunk, expected_hash, best_node))
+            try:
+                ack_received = await self.send_initial_ack(reader, writer, data)
+                if ack_received:
+                    await self.send_data_to_node(writer, idx, data)
+                    break
+                else:
+                    self.node_metadata[ip]['retries'] += 1
+                    delay = self.get_backoff_delay(attempt)
+                    await asyncio.sleep(delay)
+                    attempt += 1
+            finally:
+                self.busy_nodes[ip] = False
 
-    async def distribute_chunk_to_node(self, chunk, expected_hash, ip, retries=0):
-        try:
-            success = await self.send_chunk_to_peer(chunk, expected_hash, ip)
-            if success:
-                # Increase success_rate (just a mock, replace with your actual logic)
-                node_metadata[ip]['success_rate'] += 5
-            else:
-                if retries < MAX_RETRIES:
-                    await asyncio.sleep(BACKOFF_INITIAL_DELAY * (2 ** retries))
-                    await self.distribute_chunk_to_node(chunk, expected_hash, ip, retries + 1)
-        finally:
-            busy_nodes.remove(ip)
+    async def send_initial_ack(self, reader, writer, data):
+        size = len(data)
+        message = f"type:upload,size:{size}"
+        writer.write(message.encode())
+        await writer.drain()
+        response = await reader.readline()
+        expected_hash = hashlib.sha256(str(size).encode()).hexdigest()
 
-    async def distribute_all_chunks(self, chunks_dict):
-        for chunk, expected_hash in chunks_dict.items():
-            self.distribute_chunk_to_all_peers(chunk, expected_hash)
+        return response.decode().strip() == expected_hash
 
+    async def send_data_to_node(self, writer, idx, data):
+        writer.write(data.encode())
+        await writer.drain()
 
-def main():
-    distributor = Distributor()
-    chunks_dict = {
-        "chunk1": hashlib.sha256("chunk1".encode()).hexdigest(),
-        "chunk2": hashlib.sha256("chunk2".encode()).hexdigest(),
-        # ... other chunks
-    }
+    async def distribute_to_nodes(self, data_table):
+        tasks = [self.distribute_chunk_to_node(idx, data) for idx, data in data_table.items()]
+        await asyncio.gather(*tasks)
+    async def distribute(self,data_table):
+        self.table = {}
+        for i in data_table:
+            self.table[i] = {}
+            for j in data_table[i]:
+                if j == "DATA":
+                    self.table[i][j] = base64.b64encode(data_table[i][j]).decode()
 
-    asyncio.run(distributor.distribute_all_chunks(chunks_dict))
+# Example usage
+peer_connections = {
+'192.168.1.1': (None, None),  # dummy (reader, writer) for now
+'192.168.1.2': (None, None)
+}
 
+distributor = Distributor(peer_connections)
+data_table = {
+1: 'data1',
+2: 'data2',
+3: 'data3'
+}
 
-if __name__ == "__main__":
-    main()
+# To distribute the chunks
+# asyncio.run(distributor.distribute_to_nodes(data_table))
